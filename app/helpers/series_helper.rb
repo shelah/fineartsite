c95659b6ce5e09313b61edce23dc83abd5c8f911
Shelah Horvitz <shelah@shelahhorvitz.com>
2013-01-25 14:51:47 -0500

    Got series date range to format correctly

--- app/helpers/series_helper.rb
+++ app/helpers/series_helper.rb
@@ -1,2 +1,13 @@
 module SeriesHelper
+  def format_datespan
+    str = @series.begin_date.year.to_s
+    if @series.end_date.nil?
+      str += ' - present'
+    else
+      if @series.begin_date.year != @series.end_date.year
+        str += " - " + @series.end_date.year.to_s
+      end
+    end
+    str
+  end
 end
