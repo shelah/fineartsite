6168c91a9bedc9de403ff59ad3609b9f75d99c5c
Shelah Horvitz <shelah@shelahhorvitz.com>
2013-02-28 13:43:42 -0500

    Removed signup link and set style for login links

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130123192243) do
+ActiveRecord::Schema.define(:version => 20130225184715) do
 
   create_table "artworks", :force => true do |t|
     t.string   "title"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(:version => 20130123192243) do
     t.string   "name"
     t.date     "begin_date"
     t.date     "end_date"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "salt"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
