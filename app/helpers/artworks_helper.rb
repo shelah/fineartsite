b4414074dfc3d9acc70c20f9b4af69deb6fdde7f
Shelah Horvitz <shelah@shelahhorvitz.com>
2013-03-22 12:05:04 -0400

    [rails3-upgrade] Got previous and next buttons so work and display properly

--- app/helpers/artworks_helper.rb
+++ app/helpers/artworks_helper.rb
@@ -15,13 +15,15 @@ module ArtworksHelper
 
   def conditional_prev_link
     unless @prev_id.nil?
-      link_to image_tag('previous.gif', :alt => "previous", :size => "21x11", :id => @prev_id, :controller => 'artworks', :action => 'show')
+      link_to image_tag('previous.gif', :alt => "previous", :size => "21x11"),
+              :id => @prev_id, :series_id=> @series.id, :controller => 'artworks',:action => 'show'
     end
   end
 
   def conditional_next_link
     unless @next_id.nil?
-      link_to image_tag('next.gif', :alt => "next", :size => "21x11", :id => @next_id, :controller => 'artworks', :action => 'show')
+      link_to image_tag('next.gif', :alt => "next", :size => "21x11"),
+              :id => @next_id, :series_id=> @series.id, :controller => 'artworks', :action => 'show'
     end
   end
 
